e7c2d9ed57f3d5abe227c17b30fe8e0c86d5f28f
Chris Pole <Chris.Polewarczyk@gmail.com>
2023-03-11 17:22:37 -0500

    Comments for set up of code and potential methods

--- Main.java
+++ Main.java
@@ -1,4 +1,28 @@
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+import java.io.IOException;
+
+
 public class Main {
+
+    //Calc Average Pressure call API
+
+    //Calc Average Fuel Consumption call API
+
+    //Error Catching
+
+    //Alert of Driver
+
+    //Counter Measures
+
+    // Shut Valve
+
+    // Run off battery Call API
+
+    // Reduce Pressure
+
+    // Safe Pressure
 
 
 
